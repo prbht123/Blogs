d6d185ca9b7dcef1c3d931dd75b856c5683b3b6c
Prabhat <prabhat.s@innomick.com>
2022-04-04 16:38:44 +0530

    Created serializers.py and html pages

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blogapp',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- blog/blogapp/views.py
+++ blog/blogapp/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView
 # Create your views here.
 
 def home(request):
     return render(request,'blog/home.html')
+
+class CreatePost(TemplateView):
+    template_name='blog/post/create.html'
+
+class About(TemplateView):
+    template_name='blog/about.html'
+
+class Contact(TemplateView):
+    template_name='blog/contact.html'
+
