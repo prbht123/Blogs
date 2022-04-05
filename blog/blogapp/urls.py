83f228db82a6dfa6c5d7599f79b1adf3f3071371
Prabhat <prabhat.s@innomick.com>
2022-04-05 17:43:34 +0530

    Implemented on Search fields,bloglist for authenticated user

--- blog/blogapp/urls.py
+++ blog/blogapp/urls.py
@@ -5,13 +5,16 @@ from . import views
 
 urlpatterns = [
     
-    path('', views.home,name='home'),
+    path('', views.BlogListApi.as_view(),name='home'),
     path('about/',views.About.as_view()),
     path('contact/',views.Contact.as_view()),
     #path('create/',views.CreatePost.as_view()),
     path('create/',views.AddPostView.as_view()),
     path('bloglist/',views.BlogList.as_view()),
-    path('bloglistapi/',views.BlogListApi.as_view()),
+    path('bloglistuser/',views.BlogListApiUser.as_view()),
     path('blogdetail/<int:pk>/',views.BlogDetailApi.as_view()),
-    path('blogdetailview/<slug:slug>',views.DetailedView.as_view())
+    path('blogdetailview/<slug:slug>',views.DetailedView.as_view()),
+    path('searchblog/',views.SearchBlog.as_view()),
+    path('category/',views.CategoryList.as_view()),
+    path('categoryblog/<slug:slug>',views.CategoryBlogList.as_view())
 ]
