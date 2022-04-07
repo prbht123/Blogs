b9dfa653c7fa3df61c46a6d9d399888eae1c88c6
Prabhat <prabhat.s@innomick.com>
2022-04-07 12:47:03 +0530

    added category and tags by admin site

--- blog/blogapp/models.py
+++ blog/blogapp/models.py
@@ -7,13 +7,22 @@ from taggit.managers import TaggableManager
 class Category(models.Model):
     category_name=models.CharField(max_length=100)
     slug= models.SlugField(max_length=250)
+    image = models.ImageField(upload_to='blog/category/images/',blank=True)
+    description = models.CharField(max_length=500, null=True,blank=True, verbose_name='Description')
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     def __str__(self):
         return self.category_name
 
+class Tag(models.Model):
 
-class Post(models.Model):
+    name  = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return self.name 
+
+
+class Blog(models.Model):
     STATUS_CHOICES = (
     ('draft', 'Draft'),
     ('published', 'Published'),
@@ -23,6 +32,7 @@ class Post(models.Model):
     author = models.ForeignKey(User,on_delete = models.CASCADE,related_name='blog_posts')
     body = models.TextField()
     category=models.ForeignKey(Category,on_delete = models.CASCADE,related_name='category_posts')
+    tags = models.ManyToManyField(Tag, blank=True)
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
@@ -40,7 +50,7 @@ class Post(models.Model):
 
 
 class PostImages(models.Model):
-    post=models.ForeignKey(Post,on_delete = models.CASCADE,related_name='image_posts')
+    post = models.ForeignKey(Blog,on_delete = models.CASCADE,related_name='image_posts')
     image = models.ImageField(upload_to='blog/images/',blank=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
@@ -48,3 +58,10 @@ class PostImages(models.Model):
     def __str__(self):
         return self.post.title
 
+class Contact(models.Model):
+    name = models.CharField(max_length=100, null=True, verbose_name='Name')
+    email = models.EmailField(null=True)
+    mobile_number = models.BigIntegerField()
+    messages = models.TextField()
+    def __str__(self):
+        return f"{ self.name }" 
